742e1e0b317610952553b6928b74bdc00e43814f
Michele Dexter <micheleannedexter@gmail.com>
2019-07-15 08:49:41 -0400

    add cors filter

---
+++ server/middleware/cors-filter.middleware.js
@@ -0,0 +1,7 @@
+module.exports = function corsFilter(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  next();
+}
